8e505daf55be14ccebb4df14b23688eeb1bbbfe5
Grant Olsen <jython.scripts@gmail.com>
2019-09-14 05:36:19 -0400

    Initial commit v2.0.2

---
+++ Project/Assets/AnimPrep/Scripts/CreateAssetBundles.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+
+public class CreateAssetBundles
+{
+	[MenuItem ("Assets/Build AssetBundles")]
+	static void BuildAllAssetBundles ()
+	{
+		BuildPipeline.BuildAssetBundles ("Assets/AssetBundles", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+	}
+
+	[MenuItem ("Assets/Remove Undersea")]
+	static void RemoveName ()
+	{
+		var names = AssetDatabase.GetAllAssetBundleNames();
+		foreach (var name in names) {
+			if (name == "tree.scene") {
+				AssetDatabase.RemoveAssetBundleName ("tree.scene", true);
+				Debug.Log ("Found and removed");
+			}
+			Debug.Log ("AssetBundle: " + name);
+		}
+	}
+}
+
+
+#endif
---
+++ Project/Assets/AssetBundles/readme.txt
@@ -0,0 +1,6 @@
+In Unity's menu bar locat: "Assets>Build AssetBundles" to build the scene to an assetbundle. Then,
+
+In this assetbundle folder contains the useful files that can be transfered into Animation Prep Studio. 
+This folder contains the *.scene files that should be placed into the folder:
+
+%USERPROFILE%\LocalLow\Animation Prep Studios\AnimationPrepStudio_Lite\VR_MocapAssets
